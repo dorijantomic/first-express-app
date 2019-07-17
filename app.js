3a121752919b32c4a8fd5176e195b553848845d8
xdorijanx <dorijan11@gmail.com>
2019-07-17 18:58:05 +0200

    Refactored the node project to use express js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
- const http = require('http');
 
-
-/*
- The createServer method allows us to create a server, it requires a request and a response.
- When a request is received the response will be triggered
- the request and response of our server constant is now handled inside of routes 
- so when a request to the server is made the routes handler function will run and manage the requests
-
-*/
- const server = http.createServer(); // The code is now cleaner as all the routing is switched into it's own file
-
-// The server that we've made above requires a method to be callede on it which tells it to listen for users that enter localhost(3000) <-- we define the port
- server.listen(3000)
+ const express = require('express')
+ 
+ const app = express()
+// use allows us to add a new middleware function that runs on each request
+app.use((req,res,next) => {
+    console.log('In the Middleware!')
+    next() // allows the next Middleware to be executed
+})
+app.use((req,res,next) => {
+    console.log('In another Middleware!')
+    res.send(`<h1> Hello From Express </h1>`)
+})
+app.listen(3000) // creates a server and listens for requests 
