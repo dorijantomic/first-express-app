2a49a749e5e56161218aae6796643d78cbaf06aa
xdorijanx <dorijan11@gmail.com>
2019-07-17 19:32:25 +0200

    Added separate folder for routes and use router in those individual folder/files

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 
- const express = require('express')
- 
- const app = express()
-// use allows us to add a new middleware function that runs on each request
-app.use((req,res,next) => {
-    console.log('In the Middleware!')
-    next() // allows the next Middleware to be executed
-})
-app.use((req,res,next) => {
-    console.log('In another Middleware!')
-    res.send(`<h1> Hello From Express </h1>`)
-})
-app.listen(3000) // creates a server and listens for requests 
+const express = require("express");
+
+const adminRoutes = require('./routes/admin')
+const shop = require('./routes/shop')
+
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false})) // parses the body and runs next so it reaches other middleware
+
+app.use(adminRoutes);
+
+app.use(shop)
+
+app.use("/", (req, res, next) => {
+  res.send(`<h1> Hello From Express </h1>`);
+});
+
+app.listen(3000); // creates a server and listens for requests
