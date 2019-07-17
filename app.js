2e253bae8a110717a05ab4981678f0230dec62e0
xdorijanx <dorijan11@gmail.com>
2019-07-17 18:29:03 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  const http = require('http');
- const routes = require('./routes'); // We import the route handling from the routes file
+
 
 /*
  The createServer method allows us to create a server, it requires a request and a response.
@@ -8,7 +8,7 @@
  so when a request to the server is made the routes handler function will run and manage the requests
 
 */
- const server = http.createServer(routes.handler); // The code is now cleaner as all the routing is switched into it's own file
+ const server = http.createServer(); // The code is now cleaner as all the routing is switched into it's own file
 
 // The server that we've made above requires a method to be callede on it which tells it to listen for users that enter localhost(3000) <-- we define the port
  server.listen(3000)
